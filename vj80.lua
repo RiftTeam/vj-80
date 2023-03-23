fdf995b8db0022e2b4251659e7c833568ffe6dae
roceal <ronan.oceallaigh@gmail.com>
2023-03-23 02:08:35 +0000

    bunch of effects and improved fft

--- vj80.lua
+++ vj80.lua
@@ -1,3 +1,9 @@
+-- title:  mt's vj rig
+-- author: mantratronic
+-- desc:   collection of effects made for the monday night tic80 jams on twitch.tv/fieldfxdemo
+-- script: lua
+
+
 -- in
 --[[
 logo={"  `___//  __//_ __//___//_---//___;---//__;--\\\\__.",
@@ -10,9 +16,8 @@ if (beat/4%4 > 1) then ta = 0 else ta = s(pi*ta) end
  println(44-ta*24,58-ta*1.5,3+ta,3+ta,0+beat*2)
 
 --]]
-
-
-
+--[[
+--]]
 X = 7
 function X_BOOT()
 end
@@ -20,11 +25,11 @@ end
 function X_DRAW(it,ifft)
 end
 
-NumEffects=5
-NumOverlays=4
-NumModifiers=2
+NumEffects=8
+NumOverlays=8
+NumModifiers=4
 NumModes=8
-NumPalettes=3
+NumPalettes=5
 
 -- Utils
 
@@ -75,7 +80,7 @@ end
 
 FFTH={}
 FFTC={}
-FFTH_length=4
+FFTH_length=20
 BASS=0
 BASSC=0
 BASSDIV=10
@@ -124,10 +129,75 @@ end
 
 -- Effects
 
+SwirlTunnel = 8
+
+function SwirlTunnel_DRAW(it,ifft)
+  it=it/10
+  k=sin(it*tau)*99
+  l=sin(it*tau*2)*49
+  for i=0,32639 do
+  x=i%240-k-120
+  y=i/240-l-68
+  u=m.atan2(y,x)
+  d=(x*x+y*y)^.5
+  v=99/d
+--  c=sin(v+sin((u+sin(v)*sin(it/9)*2)*tau)+it)
+  c=sin(v+(u+sin(v)*sin(ifft/4)*tau)+t/1000)+1
+  poke4(i,mm(c*8-c*((138-d)/138),0,15))
+  end
+  --circ(k+120,l+68,9+5*sin(it/9),EControl)
+end
+
+CloudTunnel = 7
+function CloudTunnel_BOOT()
+end
+
+function CloudTunnel_DRAW(it,ifft)
+ for i=0,32639 do
+  x=i%240-120
+  y=i//240-68
+  s=sin(it)
+  c=cos(it)
+  k=(x*s-y*c)%40-20
+  l=(y*s+x*c)%40-20
+  d=(x*x+y*y)^.5
+  a=math.atan2(y,x)
+  e=(k*k+l*l)^.5
+  c=((99/d)*(e/30+sin(it)+ ifft)-a*2.55 )%8+EControl
+  poke4(i,c)
+ end
+end
+
+TunnelWall = 6
+
+function TunnelWall_DRAW(it,ifft)
+  it=it/2
+ for x=0,239 do
+  for y=0,135 do
+    sx=x-120*sin(it)
+    sy=y-68 
+    r=99+50*sin(it/3) - EControl*2
+    s=sin(it)
+    c=cos(it)
+    X=(sx*s-sy*c)
+    Y=(sy*s+sx*c)
+    k=X%r-r/2
+    l=Y%r-r/2
+    a=math.atan2(k,l)
+    e=(k*k+l*l)^.5  
+    K=X//r 
+    L=Y//r 
+    ff = mm(abs(K+L)//1 + 10,0,255)
+    ff = FFTH[ff]*.2+K
+    pix(x,y,((99/e)*2*sin(it*ff+K+L)-a*2.55)%(8)+K+L*4)
+  end
+ end
+end
+
 Quup = 5
 
 function Quup_DRAW(it,ifft)
- tt=it/8
+ tt=it/8 * EControl
  P=3+tt//5%5
  Q=P/2
  I=tt/15%1
@@ -272,7 +342,7 @@ end
 function FujiTwist_DRAW(it,ifft)
  bnc=sin(ifft)*10
  for y=1,Fuji_height do
-  twist=Fuji_numframes*(1+cos(it/4*tau))/2
+  twist=Fuji_numframes*(1+cos(it/4*tau)+BASS/4*cos(y/Fuji_height))/2
   dl=Fuji_frames[twist//1%Fuji_numframes+1]
   for i=1,#dl do
    ln=dl[i]
@@ -357,6 +427,25 @@ function TwistFFT_DRAW(it,ifft)
 end
 
 -- Modifiers
+
+OOrder = 0
+EOrder = 0
+function ModifierHandler(COrder,IOrder,Mod, MT,MC)
+  if COrder == IOrder then
+    if Mod == 0 then
+    elseif Mod == PIXNoise then
+     PIXNoise_DRAW(1000*HIGH)
+    elseif Mod == PIXZoom then
+     PIXZoom_DRAW(1000*HIGH)
+    elseif Mod == GridDim then
+     GridDim_DRAW(1000*HIGH, MT,MC)
+    elseif Mod == POSTRibbons then
+     POSTRibbons_DRAW(1000*HIGH, MT,MC)
+    end
+  else
+  end
+end
+
 PIXNoise = 1
 function PIXNoise_DRAW(amount)
  for i=0,amount do
@@ -383,6 +472,66 @@ function PIXZoom_DRAW(amount)
   end
  end
 end
+
+GridDim = 3
+
+function GridDim_DRAW(amount, mt, mc)
+ i=0
+ for y=-1,36 do
+  for x=-1,60 do
+   i=i+1
+   if i > amount*5 then return end
+   sx=x*4+(mt*20)%4
+   sy=y*4+(mt*5)%4
+   pix(sx,sy,mm(pix(sx,sy)-1,0,255))
+  end
+ end
+end
+
+POSTRibbons = 4
+
+function POSTRibbons_DRAW(amount, mt,mc)
+  it=mt*40
+  lim = mm(8+mc,0,15)
+  for y=0,135 do 
+   for x=0,239 do
+    Y=(68-y)
+    X=(120-x)
+    d=math.sqrt(X^2+Y^2)//1
+    a=math.atan2(X,Y)
+    --a=a*d//4
+    --c=(abs(68-y)+abs(120-x)+it)//20%4+12
+    if((10*sin(it/4+d/5))%100>50) then
+     c=pix(x,y)
+     if c>lim then
+      pix(x,y,mm(c-8,0,15))
+     end
+    end 
+   end 
+  end
+end
+--[[
+for y=0,136 do for x=0,240 do
+Y=(68-y+t)*y/1000+t
+X=(120-x+t)*x/2000+t
+h=math.sqrt(X^2+Y^2)//1
+g=math.atan(X,Y)*10
+g=g*h//4
+c=(math.abs(68-y)+math.abs(120-x)+t)//20%4+12
+if((g+10*math.sin(t/4+h/5))%100>50) then
+if c>14 then
+pix(x,y,h+(g&h)&t//1%4)
+elseif c<14 and c>=13 then
+pix(x,y,(x+y+t)/4%4+8)
+else
+pix(x,y,0)
+end
+else
+pix(x,y,c)
+end
+end end 
+  
+--]]
 
 -- Overlays
 text={"LOVEBYTE", "10-12th FEB", "Online", "lovebyte.party"}
@@ -426,7 +575,8 @@ function SmilyFaces_BOOT()
 end
 
 function SmilyFaces_DRAW(it,ifft)
- for i=1,NumSmilyFaces do
+ nsf = mm(OControl,1,NumSmilyFaces)
+ for i=1,nsf do
   local sm=SmilyFaces_xysr[i]
   x=(sm[1])%300 - 30
   y=(sm[2]+FFTC[i]*2*i^0.8)%200 - 32
@@ -500,8 +650,67 @@ function TextWarp_DRAW(it,ifft)
   w=pp[5]/2+10*sin(pp[5]/40*MID+it/99)+(it/111)%5
   nx=w*sin(b)
   ny=w*cos(b)
-  pix(120+nx,68+ny,pp[3])
+  pix(120+nx,68+ny,OControl)-- pp[3])
  end
+end
+
+Snow = 5
+function Snow_BOOT()
+end
+function Snow_DRAW(it,ifft)
+  for i=0,OControl do
+    circ(rand(240),rand(136),rand(4),it)
+  end
+end
+
+SmokeCircles = 6
+SC_p={}
+SC_np = 99
+function SmokeCircles_BOOT()
+  for i=0,SC_np do
+    SC_p[i]={x=4-8*rand(),y=4-8*rand(),z=20*rand()}
+  end
+end
+
+function SmokeCircles_DRAW(it,ifft)
+  it=it*5
+  for i=0,SC_np do
+    z=(SC_p[i].z+it)%20
+    x=SC_p[i].x
+    y=SC_p[i].y
+    t2=-(1-z/9)
+    X=x*cos(t2)-y*sin(t2)
+    Y=y*cos(t2)+x*sin(t2)
+    circ(120+X*z,68+Y*z,20-z,15-(z/1.5))
+  end
+end
+
+Spiral = 7
+
+function Spiral_DRAW(it,ifft)
+it=it*30
+ for i=0,200 do
+  j=(i/10+it)%120
+  i2=i/20
+  i2=i2*i2
+  z=j+i2
+  X=sin(j)*z
+  Y=cos(j)*z
+  circ(120+X,68+Y,z/10-OControl*2,mm(15*j/120,0,15))
+ end
+end
+
+Bobs = 8
+function Bobs_DRAW(it,ifft)
+  for i=0,99 do
+    j=i/12
+    x=10*sin(pi*j+it)
+    y=10*cos(pi*j+it)
+    z=10*sin(pi*j)
+    X=x*sin(it)-z*cos(it)
+    Z=x*cos(it)+z*sin(it)
+    circ(120+X*Z,68+y*Z,Z,OControl*MID)
+  end
 end
 
 
@@ -580,11 +789,17 @@ BlueOrangePAL={}
 
 Reddish=2
 ReddishPAL={}
+
+BlueGreySine=5
+BlueGreySinePAL={}
+
 function PAL_BOOT()
   for i=0,47 do
     Sweetie16PAL[i]=peek(0x3fc0+i)
 
     BlueOrangePAL[i]=mm(sin(i)^2*i,0,255)
+
+    BlueGreySinePAL[i]=sin(i/15)*255
   end
 
   for i=0,15 do
@@ -635,6 +850,18 @@ function PAL_Rotate1(it,l)
   end
 end
 
+Dutch = 4
+function PAL_Rotate2(it,l)
+  grader=sin(it*1/7+l/150)+1
+  gradeg=sin(it*1/13+l/150)+1
+  gradeb=sin(it*1/11+l/150)+1
+  for i=0,15 do
+   poke(0x3fc0+i*3,  mm(i*16*(grader),0,255))
+   poke(0x3fc0+i*3+1,mm(i*16*(gradeg),0,255))
+   poke(0x3fc0+i*3+2,mm(i*16*(gradeb),0,255))
+  end
+end
+
 function PAL_Handle(np,l)
  if np == Sweetie16 then
   PAL_Switch(Sweetie16PAL,0.1)
@@ -643,7 +870,11 @@ function PAL_Handle(np,l)
  elseif np == Reddish then
   PAL_Switch(ReddishPAL,0.1)
  elseif np == Pastels then
-  PAL_Rotate1(ET,l)
+  PAL_Rotate1(t/BT,l)
+ elseif np == Dutch then
+  PAL_Rotate2(t/BT,l)
+ elseif np == BlueGreySine then
+  PAL_Switch(BlueGreySinePAL,0.1)
  end
 end
 
@@ -660,6 +891,9 @@ EControl = 1
 OControl = 1
 EPalette = 0
 OPalette = 0
+
+EMControl = 1
+OMControl = 1
 
 -- ie, beat, BASS, pos/neg
 ETimerMode=0
@@ -716,15 +950,24 @@ function KEY_CHECK()
  
  -- home: Loudness up by 0.1
  if keyp(56) then
-  Loudness = Loudness + 0.1
+  if key(64) then -- shift, change FFTH_length by 1
+    FFTH_length = FFTH_length + 1
+  else
+   Loudness = Loudness + 0.1
+  end
  end
 
  -- end: Loudness down by 0.1
  if keyp(57) then
-  Loudness = Loudness - 0.1
+  if key(64) then -- shift, change FFT_Length by 1
+    FFTH_length = FFTH_length - 1
+  else
+   Loudness = Loudness - 0.1
+  end
  end
 
  Loudness = mm(Loudness,0.1,10)
+ FFTH_length = mm(FFTH_length,2,60)
 
  -- insert: effect cls switch
  if keyp(53) == true then
@@ -732,6 +975,15 @@ function KEY_CHECK()
    ECLS = false
   else
    ECLS = true
+  end
+ end
+
+ -- pageup: effect modifier order switch
+ if keyp(54) == true then
+  if EOrder == 0 then 
+    EOrder = 1
+  else
+    EOrder = 0
   end
  end
  
@@ -804,7 +1056,17 @@ function KEY_CHECK()
  end
  
  EModifier = mm(EModifier,0,NumModifiers)
+ 
+ -- 3: effect modifier control down
+ if keyp(30) == true then
+  EMControl = EMControl - 1
+ end
 
+ -- 4: effect modifier control up
+ if keyp(31) == true then
+  EMControl = EMControl + 1
+ end
+ 
  -- z: overlay modifier down
  if keyp(26) == true then
   OModifier = OModifier - 1
@@ -817,6 +1079,16 @@ function KEY_CHECK()
  
  OModifier = mm(OModifier,0,NumModifiers)
 
+ -- c: overlay modifier control down
+ if keyp(3) == true then
+  OMControl = OMControl - 1
+ end
+
+ -- v: overlay modifier control up
+ if keyp(22) == true then
+  OMControl = OMControl + 1
+ end
+ 
  -- TODO: key instead of keyp? limit?
  -- d: overlay control down
  if keyp(4) == true then
@@ -871,6 +1143,15 @@ function KEY_CHECK()
    OCLS = true
   end
  end
+
+ -- pagedown: effect modifier order switch
+ if keyp(55) == true then
+  if OOrder == 0 then 
+    OOrder = 1
+  else
+    OOrder = 0
+  end
+ end
  
  -- a: overlay down
  if keyp(1) == true then
@@ -919,10 +1200,15 @@ function BOOT()
  SunBeat_BOOT()
  SmilyFaces_BOOT()
  FujiTwist_BOOT()
+ SmokeCircles_BOOT()
 end
 
 function TIC()t=time()
  vbank(0)
+
+ -- should remove mouse pointer but doesnt
+ poke(0x3ffb,4)
+
  bt=((t-LBT))/(BT)
  
  if ECLS then 
@@ -957,6 +1243,8 @@ else
   ET = -ET
  end
  
+ ModifierHandler(EOrder,0,EModifier, ET,EMControl)
+
  if Effect == 0 then
   VolTest_DRAW(t,t)
  elseif Effect == TwistFFT then
@@ -967,16 +1255,17 @@ else
   FujiTwist_DRAW(ET,MID)
  elseif Effect == ATTunnel then
   ATTunnel_DRAW(ET,MID)
-elseif Effect == Quup then
+ elseif Effect == Quup then
   Quup_DRAW(ET,MID)
+ elseif Effect == TunnelWall then
+  TunnelWall_DRAW(ET,MID)
+ elseif Effect == CloudTunnel then
+  CloudTunnel_DRAW(ET,MID)
+elseif Effect == SwirlTunnel then
+  SwirlTunnel_DRAW(ET,BASS)
  end
 
- if EModifier == 0 then
- elseif EModifier == PIXNoise then
-  PIXNoise_DRAW(1000*HIGH)
- elseif EModifier == PIXZoom then
-  PIXZoom_DRAW(1000*HIGH)
- end
+ ModifierHandler(EOrder,1,EModifier, ET,EMControl)
 
  vbank(1)
  if OCLS then 
@@ -1008,6 +1297,8 @@ elseif OTimerMode == THIGH then
   OT = -OT
  end
 
+ ModifierHandler(OOrder,0,OModifier, OT,OMControl)
+
  if Overlay == 0 then
  elseif Overlay == TextBounceUp then
   TextBounceUp_DRAW(OT,OT)
@@ -1017,20 +1308,23 @@ elseif OTimerMode == THIGH then
   SmilyFaces_DRAW(OT,OT)
  elseif Overlay == TextWarp then
   TextWarp_DRAW(OT,OT)
+ elseif Overlay == Snow then
+  Snow_DRAW(OT,OT)
+ elseif Overlay == SmokeCircles then
+  SmokeCircles_DRAW(OT,OT)
+ elseif Overlay == Spiral then
+  Spiral_DRAW(OT,OT)
+elseif Overlay == Bobs then
+  Bobs_DRAW(OT,OT)
  end
 
- if OModifier == 0 then
- elseif OModifier == PIXNoise then
-  PIXNoise_DRAW(1000*HIGH)
- elseif OModifier == PIXZoom then
-  PIXZoom_DRAW(1000*HIGH)
- end
+ ModifierHandler(OOrder,1,OModifier, OT,OMControl)
 
  if DEBUG == true then
   print("Effect: "..Effect.."|Ctrl: "..EControl.."|Timer: "..ETimerMode.."|Sped: "..EDivider.."|Pal: "..EPalette,0,100,12)
-  print("EModifier: "..EModifier,0,108,12)
+  print("EModifier: "..EModifier.."|Ctrl: "..EMControl,0,108,12)
   print("Overlay: "..Overlay.."|Ctrl: "..OControl.."|Timer: "..OTimerMode.."|Sped: "..ODivider.."|Pal: "..OPalette,0,116,12)
-  print("OModifier: "..OModifier,0,124,12)
+  print("OModifier: "..OModifier.."|Ctrl: "..OMControl,0,124,12)
  end
 end
 
